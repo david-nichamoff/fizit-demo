38aa8177043ec8c0ec793ec0c52768cd01e6a28e
David Nichamoff <david@fizit.biz>
2024-05-17 14:35:12 -0400

    troubleshoot

--- packages/env_var.py
+++ packages/env_var.py
@@ -7,8 +7,6 @@ def get_env():
     global _env_var
     python_path = os.environ['PYTHONPATH']
 
-    print ("Loading env")
-
     if _env_var is None:
         with open(python_path + "/../config/private_keys.json") as cf:
             _env_var = json.load(cf)
@@ -17,5 +15,4 @@ def get_env():
         with open(python_path + "/../truffle/build/contracts/Delivery.json") as cf:
             _env_var["contract_abi"] = json.load(cf)["abi"]
 
-    print (_env_var)
     return _env_var
