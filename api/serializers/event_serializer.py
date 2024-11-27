2b76c5f02db040270ae3d7358ecf0153d5f00062
David Nichamoff <david@fizit.biz>
2024-11-27 10:08:45 -0500

    fix to events

--- api/serializers/event_serializer.py
+++ api/serializers/event_serializer.py
@@ -5,4 +5,4 @@ from api.models.event_models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event 
-        fields = ['event_idx', 'contract_idx', 'contract_addr', 'event_type', 'details', 'event_dt']
+        fields = ['event_idx', 'contract_idx', 'network', 'from_addr', 'to_addr', 'tx_hash', 'gas_used', 'event_type', 'event_dt', 'details', 'status']
--- api/views/event_views.py
+++ api/views/event_views.py
@@ -26,7 +26,8 @@ class EventViewSet(viewsets.ViewSet):
         tags=["Events"],
         parameters=[
             OpenApiParameter(name='contract_idx', description='Contract_idx for filtering contract events', required=False, type=int),
-            OpenApiParameter(name='contract_addr', description='Contract address for filtering contract events', required=False, type=str),
+            OpenApiParameter(name='from_addr', description='Contract address for filtering contract events', required=False, type=str),
+            OpenApiParameter(name='to_addr', description='Contract address for filtering contract events', required=False, type=str),
         ],
         summary="List Events",
         description="Retrieve the list of events"
@@ -34,13 +35,16 @@ class EventViewSet(viewsets.ViewSet):
     def list(self, request):
         try:
             contract_idx = request.query_params.get('contract_idx')
-            contract_addr = request.query_params.get('contract_addr')
+            from_addr = request.query_params.get('from_addr')
+            to_addr = request.query_params.get('to_addr')
             queryset = Event.objects.all()
 
             if contract_idx:
                 queryset = queryset.filter(contract_idx=contract_idx)
-            if contract_addr:
-                queryset = queryset.filter(contract_addr=contract_addr)
+            if from_addr:
+                queryset = queryset.filter(from_addr=from_addr)
+            if to_addr:
+                queryset = queryset.filter(to_addr=to_addr)
 
             serializer = EventSerializer(queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
