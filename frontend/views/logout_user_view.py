ae9fbf37e257094d4077dae4f22f9d4dde610388
David Nichamoff <david@fizit.biz>
2025-04-11 10:40:44 -0400

    change logout behavior

--- frontend/views/logout_user_view.py
+++ frontend/views/logout_user_view.py
@@ -6,10 +6,9 @@ from urllib.parse import urlencode
 from api.utilities.logging import log_info, log_warning, log_error
 
 logger = logging.getLogger(__name__)
-
 def oidc_logout(request):
 
     logger.info("📤 OIDC logout view called for user: %s", request.user)
     logout(request)
     logger.info("✅ Django session logged out, redirecting to Google logout")
-    return redirect("https://accounts.google.com/Logout")
+    return redirect('/')
